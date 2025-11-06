1a20c525312160c4dcd70944f2c3a3ecf640a412
Oscar <oscalberto03@gmail.com>
2025-11-06 15:43:18 -0500

    Actualizar conexión a BD en la nube

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
-import { config } from 'dotenv'
+import {config} from 'dotenv'
 config()
 
-export const BD_HOST = process.env.BD_HOST || 'bd20252.infinityfreeapp.com'
-export const BD_DATABASE = process.env.BD_DATABASE || 'if0_40242567_bdnueva'
-export const BD_USER = process.env.BD_USER || 'if0_40242567'
-export const BD_PASSWORD = process.env.BD_PASSWORD || 'Ingsds3BF3'
-export const BD_PORT = process.env.BD_PORT || 3306
-export const PORT = process.env.PORT || 3000
-export const SECRET = process.env.SECRET || 'oscarELPROXD'
+export const BD_HOST=process.env.BD_HOST || 'bqjrswyn6p04x0cw7qpd-mysql.services.clever-cloud.com'
+export const BD_DATABASE=process.env.BD_DATABASE || 'bqjrswyn6p04x0cw7qpd'
+export const BD_USER=process.env.BD_USER ||'ucmwltyqvkfigzbx'
+export const BD_PASSWORD=process.env.BD_PASSWORD || 'M2GhgWuoSi4enPo6xnfM'
+export const BD_PORT=process.env.BD_PORT || 3306
+export const PORT=process.env.PORT || 3000
