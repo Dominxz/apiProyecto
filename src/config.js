930da687c578de7f12cf3a54f1b28a6f7ee4e3d4
Oscar <oscalberto03@gmail.com>
2025-11-09 10:51:11 -0500

    Actualizar la bd

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import {config} from 'dotenv'
 config()
 
-export const BD_HOST=process.env.BD_HOST || '127.0.0.1'
-export const BD_DATABASE=process.env.BD_DATABASE || 'bdapi20252'
-export const BD_USER=process.env.BD_USER ||'root'
-export const BD_PASSWORD=process.env.BD_PASSWORD || 'root'
+export const BD_HOST=process.env.BD_HOST || 'bqjrswyn6p04x0cw7qpd-mysql.services.clever-cloud.com'
+export const BD_DATABASE=process.env.BD_DATABASE || 'bqjrswyn6p04x0cw7qpd'
+export const BD_USER=process.env.BD_USER ||'ucmwltyqvkfigzbx'
+export const BD_PASSWORD=process.env.BD_PASSWORD || 'M2GhgWuoSi4enPo6xnfM'
 export const BD_PORT=process.env.BD_PORT || 3306
 export const PORT=process.env.PORT || 3000
