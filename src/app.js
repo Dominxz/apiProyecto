0d79ec727fcc4454e5e0eed8b06dffd4ad7906ec
Oscar <oscalberto03@gmail.com>
2025-11-09 18:28:02 -0500

    Mas espacio para imagenes

--- src/app.js
+++ src/app.js
@@ -1,31 +1,36 @@
 import express from 'express'
 import cors from 'cors'
-//importar las rutas OJO
+
+// importar las rutas OJO
 import clientesRoutes from './routes/clientes.routes.js'
 import productosRoutes from './routes/productos.routes.js'
 import usuariosRoutes from './routes/usuarios.routes.js'
 import pedidosRoutes from './routes/pedidos.routes.js'
 
-
 const app = express();
-app.use(express.json());
-const corsOption={
-    origin:'*',
-    methods:['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials:true
-}
-app.use(cors(corsOption))
 
-//indicar las rutas a utilizar OJO
-app.use('/api',clientesRoutes)
-app.use('/api', productosRoutes)
-app.use('/api', usuariosRoutes)
-app.use('/api', pedidosRoutes)
+// 👇 Aumentamos el límite de tamaño permitido en el body
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-app.use((req,resp,next)=>{
-    resp.status(400).json({
-        message:'Endpoin not fund'
-    })
-})
+const corsOption = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true
+};
+app.use(cors(corsOption));
+
+// indicar las rutas a utilizar OJO
+app.use('/api', clientesRoutes);
+app.use('/api', productosRoutes);
+app.use('/api', usuariosRoutes);
+app.use('/api', pedidosRoutes);
+
+// manejo de rutas no encontradas
+app.use((req, resp, next) => {
+  resp.status(400).json({
+    message: 'Endpoint not found'
+  });
+});
 
 export default app;
