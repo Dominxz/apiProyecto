7d734a0b200e68e4b1b91974736ec236bb3023a1
Oscar <oscalberto03@gmail.com>
2025-11-09 20:38:19 -0500

    Cambio para imagenes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-// importar las rutas
+// importar las rutas OJO
 import clientesRoutes from './routes/clientes.routes.js'
 import productosRoutes from './routes/productos.routes.js'
 import usuariosRoutes from './routes/usuarios.routes.js'
 import pedidosRoutes from './routes/pedidos.routes.js'
 
 const app = express();
-
-// Para obtener __dirname en ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // 👇 Aumentamos el límite de tamaño permitido en el body
 app.use(express.json({ limit: '10mb' }));
@@ -26,10 +20,7 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 
-// 👉 Servir la carpeta uploads como pública
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// indicar las rutas a utilizar
+// indicar las rutas a utilizar OJO
 app.use('/api', clientesRoutes);
 app.use('/api', productosRoutes);
 app.use('/api', usuariosRoutes);
