b4ab8cceb8abb3129887041a20b8d3c2aaf49936
Oscar <oscalberto03@gmail.com>
2025-11-09 20:39:26 -0500

    Cambio para imagenes2

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,23 +1,14 @@
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
 
 // Para obtener el directorio actual en ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Ruta absoluta a la carpeta 'uploads' en el root del proyecto
-const uploadPath = path.join(__dirname, '../uploads');
-
-// Crear la carpeta si no existe
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
-
 // Configurar destino y nombre del archivo
 const storage = multer.diskStorage({
-  destination: uploadPath,
+  destination: path.join(__dirname, '../uploads'), // Carpeta destino
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + '-' + file.originalname;
     cb(null, uniqueName);
