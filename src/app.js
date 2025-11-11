ca7ddb2fbcc399b4a009efea02c988aa3311be30
Oscar <oscalberto03@gmail.com>
2025-11-10 23:30:20 -0500

    cambiosnuevos

--- src/app.js
+++ src/app.js
@@ -13,12 +13,41 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-const corsOption = {
-  origin: '*',
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Permitimos solicitudes sin origin (como apps móviles)
+    if (!origin) return callback(null, true);
+    const allowedOrigins = [
+      'https://apiproyecto20252.onrender.com',
+      'http://localhost:8100', // Ionic local
+      'capacitor://localhost', // App Android/iOS
+      'http://localhost'       // WebView Android
+    ];
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true
 };
-app.use(cors(corsOption));
+
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
 
 // indicar las rutas a utilizar OJO
 app.use('/api', clientesRoutes);
